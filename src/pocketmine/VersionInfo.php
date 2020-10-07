2969a459f43237234e940fded6db78065e2818f0
QMoon10189 <72264393+QMoon10189@users.noreply.github.com>
2020-10-07 22:28:32 +0800

    Versioninfo.php

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -32,7 +32,7 @@ if(defined('pocketmine\_VERSION_INFO_INCLUDED')){
 }
 const _VERSION_INFO_INCLUDED = true;
 
-const NAME = "Foxel";
+const NAME = "Lextus";
 const BASE_VERSION = "3.12.6";
-const IS_DEVELOPMENT_BUILD = false;
+const IS_DEVELOPMENT_BUILD = true;
 const BUILD_NUMBER = 0;
